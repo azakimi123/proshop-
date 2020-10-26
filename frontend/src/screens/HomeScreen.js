0231a8e4ec36b8eb42d1c473c9a3d5bf479f26e8
azakimi123 <aaron.zakimi@gmail.com>
2020-10-26 15:03:33 -0600

    loading and message component added to ternary

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 // import products from '../products';
 // import axios from 'axios'; //we are using redux
-import Product from '../components/Product';
-import { Row, Col } from 'react-bootstrap';
-import { listProducts } from '../actions/productActions'
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { Row, Col } from "react-bootstrap";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const productList = useSelector(state => state.productList)
-  const { loading, error, products} = productList;
-
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts())
-  }, [dispatch])
+    dispatch(listProducts());
+  }, [dispatch]);
 
   // const products = []
   // useEffect(() => {
@@ -31,17 +32,21 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
-      {loading ? <h2>Loading...</h2> : error ? <h3>{error}</h3> 
-      : <Row className=''>
-       {products.map(product => (
-         <Col key={product._id} sm={12} md={6} lg={4} xl={3} className='' >
-           <Product product={product}/>
-         </Col>
-       ))}
-       </Row>}
-
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row className="">
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3} className="">
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
