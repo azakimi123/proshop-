3ee22da8ffcf5951d43518b4a9ca082044bb43f6
azakimi123 <aaron.zakimi@gmail.com>
2020-10-26 13:19:59 -0600

    middleware added to handle errors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const products = require('./data/products')
-const dotenv = require('dotenv')
-const connectDB = require('./config/db')
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+const errorMid = require('./middleware/errorMiddleware');
 
 dotenv.config()
 
@@ -9,19 +10,26 @@ connectDB()
 
 const app = express();
 
+//example of how to set up a middleware
+// app.use((req, res, next) => {
+//   console.log('Hello')
+//   next()
+// })
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+//fallback for 404 errors, not an actual route
+app.use(errorMid.notFound)
+
+//error middleware
+app.use(errorMid.errorHandler)
 
 const PORT = process.env.PORT
+
+// console.log(errorMid)
 
 app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode, on port ${PORT}`))
