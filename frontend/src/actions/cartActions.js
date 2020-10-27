adb783b9b7814b935fee65fd94c3d27b16886e9a
azakimi123 <aaron.zakimi@gmail.com>
2020-10-27 16:10:44 -0600

    add item to cart action created

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+
+//getState allows us to get the whole state tree
+//ex. getState.productDetails 
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`)
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty
+    }
+  })
+
+  //local storage only accepts strings, so we are using stringify and then we will parse it back when retrieving information
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+} 
+
+export const removeFromCart  = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
