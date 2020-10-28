e222045adcada0f6d1f99354aac84407a8384efd
azakimi123 <aaron.zakimi@gmail.com>
2020-10-28 17:30:10 -0600

    get request taken out and moved to controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,12 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler')
-const { builtinModules } = require('module');
+// const asyncHandler = require('express-async-handler') //moved to product controller file
 const router = express.Router();
-const Product = require('../models/productModel');
+// const Product = require('../models/productModel'); //moved to product controller file
+const productCTRL = require('../controllers/productController');
 
-// @desc fetch all products
-// @route GET /api/products
-// @access Public
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
-  // throw new Error('you have an error')
-  res.json(products)
-}))
 
-// @desc fetch single product
-// @route GET /api/products/:id
-// @access Public
-router.get('/:id', asyncHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id)
-
-  if(product) {
-    res.json(product)
-  } else {
-    res.status(404)
-    throw new Error('Product not found')
-  }
-}))
+router.route('/').get(productCTRL.getProducts)
+router.route('/:id').get(productCTRL.getProductById)
 
 
 module.exports = router;
