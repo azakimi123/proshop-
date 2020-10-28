02f8d4aa18d7f2a1ef4d7258308c8b6b9fc81a8f
azakimi123 <aaron.zakimi@gmail.com>
2020-10-28 17:28:55 -0600

    auth middleware added to protect routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token 
+
+  //checks for the token from user
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    //decoding the token
+    try {
+      //takes the token without the 'Bearer' begining
+      token = req.headers.authorization.split(' ')[1]
+
+      const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`)
+
+      //sets the user with the data
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not Authorized, Token Failed')
+    }
+  }
+
+  if(!token){
+    res.status(401)
+    throw new Error('Not Authorized, No Token')
+  }
+
+})
+
+module.exports = protect;
