b361a3904118c0668b329409143f07979c2c7c58
azakimi123 <aaron.zakimi@gmail.com>
2020-10-29 11:26:25 -0600

    salt and hashing user info added as middleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,6 +29,20 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+//middlware that salts and hashes password when user profile is created
+//pre('save') allows this to fire before something is saved to the db
+userSchema.pre('save', async function (next) {
+  //checks that user password is not modified when they modify other fields in the future
+  //isModified('password') comes from Mongoose
+  if(!this.isModified('password')){
+    next()
+  }
+
+  //using bcrypt to secure password
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 module.exports = User;
