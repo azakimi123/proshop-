9c877f77ecf5001bd91398d2edbcb281249b5eef
azakimi123 <aaron.zakimi@gmail.com>
2020-10-29 11:26:56 -0600

    registerUser route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authCTRL = require('../controllers/userController');
 const protect = require('../middleware/authMiddleware')
 
+router.route('/').post(authCTRL.registerUser)
 router.route('/login').post(authCTRL.authUser)
 router.route('/profile').get(protect, authCTRL.getUserProfile)
 
