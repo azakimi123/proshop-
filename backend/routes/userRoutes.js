69532b3edb31c01fdaf147f74f37186b571e3a1f
azakimi123 <aaron.zakimi@gmail.com>
2020-10-30 15:11:01 -0600

    updating profile route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,7 @@ const protect = require('../middleware/authMiddleware')
 
 router.route('/').post(authCTRL.registerUser)
 router.route('/login').post(authCTRL.authUser)
-router.route('/profile').get(protect, authCTRL.getUserProfile)
+router.route('/profile').get(protect, authCTRL.getUserProfile).put(protect, authCTRL.updateUserProfile)
 
 
 module.exports = router;
