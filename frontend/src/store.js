86e8acf012e2736d75fb7d23374c5c38518445d9
azakimi123 <aaron.zakimi@gmail.com>
2020-10-30 22:00:18 -0600

    shipping address added to local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,41 +1,60 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer, productDetailsReducer } from './reducers/productReducers';
-import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdatePofileReducer } from './reducers/userReducers';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdatePofileReducer,
+} from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
-  userLogin: userLoginReducer,  
+  userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdatePofileReducer,
-})
+});
 
 //retrieving info from local storate, the cart items
-const cartItemsFromStorage = localStorage.getItem('cartItems') 
-? JSON.parse(localStorage.getItem('cartItems')) 
-: []
-
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
 //retrieving info from local storage, userInfo
-const userInfoFromStorage = localStorage.getItem('userInfo') 
-? JSON.parse(localStorage.getItem('userInfo')) 
-: null
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+//retrieving info from local storage, shipping address
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
 
 //can access local storage and cart items
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
-}
+};
 
 //allowing us to use asyc and await for axios calls for our data from db
-const middleware = [thunk]
+const middleware = [thunk];
 
 //allowing us to use dev tools for Redux
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
+export default store;
