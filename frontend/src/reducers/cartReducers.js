ffb152789fc2048207e18be1d0ef28d92f3e0a90
azakimi123 <aaron.zakimi@gmail.com>
2020-11-12 14:15:56 -0700

    car item reset potentially needed

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -3,6 +3,7 @@ import {
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
   CART_SAVE_PAYMENT_METHOD,
+  CART_ITEM_RESET,
 } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
