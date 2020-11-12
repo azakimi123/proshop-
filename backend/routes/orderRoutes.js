3cc82d4d53f1385f0dfafad4ae2ef0d726c23369
azakimi123 <aaron.zakimi@gmail.com>
2020-11-12 14:12:36 -0700

    my order routes added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,9 @@ const orderCTRL = require('../controllers/orderController');
 const protect = require('../middleware/authMiddleware')
 
 router.route('/').post(protect, orderCTRL.addOrderItems)
+router.route('/myorders').get(protect, orderCTRL.getMyOrders)
+router.route('/:id').get(protect, orderCTRL.getOrderByID)
+router.route('/:id/pay').put(protect, orderCTRL.updateOrderToPaid)
 
 module.exports = router;
 
