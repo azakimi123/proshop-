801c9824fc5721b85dff57278bb9c2b6768549c4
azakimi123 <aaron.zakimi@gmail.com>
2020-11-13 11:28:04 -0700

    get all user handler added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -118,4 +118,15 @@ const updateUserProfile = asyncHandler(async(req, res) => {
   }
 })
 
-module.exports = {authUser, registerUser, getUserProfile, updateUserProfile}
+// @desc Get all users
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = asyncHandler(async(req, res) => {
+
+  // get all users
+  const users = await User.find({})
+  res.json(users)
+
+})
+
+module.exports = { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
