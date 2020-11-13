5cdaddd044d5e284e04a2dbb91f1d4e3f44d742d
azakimi123 <aaron.zakimi@gmail.com>
2020-11-13 11:28:47 -0700

    desctructured protect, admin middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authCTRL = require('../controllers/userController');
-const protect = require('../middleware/authMiddleware')
+const {protect, admin }  = require('../middleware/authMiddleware')
+// const admin  = require('../middleware/authMiddleware')
 
+
+router.route('/').get(protect, admin, authCTRL.getUsers)
 router.route('/').post(authCTRL.registerUser)
 router.route('/login').post(authCTRL.authUser)
 router.route('/profile').get(protect, authCTRL.getUserProfile).put(protect, authCTRL.updateUserProfile)
