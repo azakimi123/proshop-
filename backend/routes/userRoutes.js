6957c1849bbfd67de484719dff3d8e54becd0570
azakimi123 <aaron.zakimi@gmail.com>
2020-11-14 04:52:29 -0700

    delete route added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authCTRL = require('../controllers/userController');
-const {protect, admin }  = require('../middleware/authMiddleware')
+const authCTRL = require("../controllers/userController");
+const { protect, admin } = require("../middleware/authMiddleware");
 // const admin  = require('../middleware/authMiddleware')
 
-
-router.route('/').get(protect, admin, authCTRL.getUsers)
-router.route('/').post(authCTRL.registerUser)
-router.route('/login').post(authCTRL.authUser)
-router.route('/profile').get(protect, authCTRL.getUserProfile).put(protect, authCTRL.updateUserProfile)
-
+router.route("/").get(protect, admin, authCTRL.getUsers);
+router.route("/").post(authCTRL.registerUser);
+router.route("/login").post(authCTRL.authUser);
+router
+  .route("/profile")
+  .get(protect, authCTRL.getUserProfile)
+  .put(protect, authCTRL.updateUserProfile);
+router.route("/:id").delete(protect, admin, authCTRL.deleteUser);
 
 module.exports = router;
