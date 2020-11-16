8d0e99aca0f78dcb328b4c2c35e53e030118605c
azakimi123 <aaron.zakimi@gmail.com>
2020-11-16 12:56:33 -0700

    delete product added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 // const asyncHandler = require('express-async-handler') //moved to product controller file
 const router = express.Router();
 // const Product = require('../models/productModel'); //moved to product controller file
-const productCTRL = require('../controllers/productController');
+const productCTRL = require("../controllers/productController");
+const { protect, admin } = require("../middleware/authMiddleware");
 
-
-router.route('/').get(productCTRL.getProducts)
-router.route('/:id').get(productCTRL.getProductById)
-
+router.route("/").get(productCTRL.getProducts);
+router
+  .route("/:id")
+  .get(productCTRL.getProductById)
+  .delete(protect, admin, productCTRL.deleteProduct);
 
 module.exports = router;
