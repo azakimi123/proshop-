0831734a8b211004174b32f48afc11166441911c
azakimi123 <aaron.zakimi@gmail.com>
2020-11-16 12:24:22 -0700

    user list and product list added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,8 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/orderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
+import ProductListScreen from './screens/ProductListScreen';
 // import testScreen from './screens/testScreen';
 
 
@@ -33,6 +35,8 @@ const  App = () => {
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
           <Route path='/admin/userlist' component={UserListScreen}/>
+          <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
+          <Route path='/admin/productlist' component={ProductListScreen}/>
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
