4deaa90c3f3af718e2fe5ddcb46509018cab0c63
azakimi123 <aaron.zakimi@gmail.com>
2020-11-17 13:02:18 -0700

    images adjusted

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,30 +1,42 @@
-import React from 'react'
-import Rating from './Rating';
-import { Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Rating from "./Rating";
+import { Card, Figure } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import './Product.css'
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded shadow'>
+    <Card className="my-3 p-3 rounded shadow">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top'/>
+        {/* <Figure>
+          <Figure.Image
+            width={160}
+            height={160}
+            alt="171x160"
+            src={product.image}
+          />
+        </Figure> */}
+        < div className='card-image-container'>
+          <Card.Img src={product.image} variant='top' className="card-image"/>
+        </div>
       </Link>
-      
+
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-        <Card.Text as='div'>
-            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
-        <Card.Text as='h3'>
-          ${product.price}
-        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
