f5b26811e7cf728c3e3eee6adfc9fa0938e46105
azakimi123 <aaron.zakimi@gmail.com>
2020-11-17 13:01:55 -0700

    theme adjusted

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import store from './store'
-import './bootstrap.min.css'
+import './bootstrap.min2.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
