ccfce9912d603c06eea89046721ca46c1fdfdd66
azakimi123 <aaron.zakimi@gmail.com>
2020-11-18 15:31:46 -0700

    orderListScreen and orderDetails added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,8 @@ import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
+import OrderListDetailsScreen from './screens/OrderListDetails';
 // import testScreen from './screens/testScreen';
 
 
@@ -38,6 +40,8 @@ const  App = () => {
           <Route path='/admin/userlist' component={UserListScreen}/>
           <Route path='/admin/user/:id/edit' component={UserEditScreen}/>
           <Route path='/admin/productlist' component={ProductListScreen}/>
+          <Route path='/admin/orderlist' component={OrderListScreen}/>
+          <Route path='/admin/order/details/:id' component={OrderListDetailsScreen}/>
           <Route path='/admin/product/:id/edit' component={ProductEditScreen}/>
           <Route path='/' component={HomeScreen} exact />
         </Container>
