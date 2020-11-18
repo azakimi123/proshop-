dbbf6661bc179350d2691401e54575e772c573e4
azakimi123 <aaron.zakimi@gmail.com>
2020-11-18 15:31:29 -0700

    get all orders/order delivered at added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const orderCTRL = require('../controllers/orderController');
-const { protect } = require('../middleware/authMiddleware')
+const { protect, admin } = require('../middleware/authMiddleware')
 
-router.route('/').post(protect, orderCTRL.addOrderItems)
+router.route('/').post(protect, orderCTRL.addOrderItems).get(protect, admin, orderCTRL.getOrders)
 router.route('/myorders').get(protect, orderCTRL.getMyOrders)
 router.route('/:id').get(protect, orderCTRL.getOrderByID)
 router.route('/:id/pay').put(protect, orderCTRL.updateOrderToPaid)
+router.route('/:id/deliver').put(protect, admin, orderCTRL.updateOrderToDelivered)
 
 module.exports = router;
 
