04e4663a104213d200b7cfdd00dc815e4770e525
azakimi123 <aaron.zakimi@gmail.com>
2020-11-19 12:56:18 -0700

    keyword, pageNumber added as args to listProducts

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,11 +20,13 @@ import {
 } from "../constants/productConstants";
 import axios from "axios";
 
-export const listProducts = () => async (dispatch) => {
+//passing in keyword for search functionality (see homescreen)
+export const listProducts = (keyword = '', pageNumber = '') => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
-    const { data } = await axios.get("/api/products");
+    //passing in keyword for search functionality
+    const { data } = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`);
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
